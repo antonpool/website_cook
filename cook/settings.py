3853c95c70595df036a28c2d76b43e417f4e244e
antonpool <kaspera@yandex.ru>
2024-02-12 18:03:23 +0300

    подключил статику кроме картинок

--- cook/settings.py
+++ cook/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR]
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
