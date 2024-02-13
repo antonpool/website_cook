ea34f729b36ea1a80f597512d7e81b6bddc07fc2
antonpool <kaspera@yandex.ru>
2024-02-13 20:36:45 +0300

    добавил первую динамическую ссылку с открыванием автоматически созданной странички (listViews)

--- main/views.py
+++ main/views.py
@@ -1,5 +1,20 @@
+from django.views.generic import ListView
 from django.shortcuts import render
+from db.models import Post
 
-# Create your views here.
-def home (request):
-    return render(request, 'base.html')
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(category__slug=self.kwargs.get("slug"))
+    # .select_related('category')
+    
+# def home(request):
+#     return render (request, 'blog/post_list.html')
+
+# class PostListView(ListView):
+#     model = Post
+
+#     def get_queryset(self):
+#         return Post.objects.filter(category__slug=self.kwargs.get("slug")).select_related('category')
